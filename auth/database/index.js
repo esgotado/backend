920e50d1fdb230a58647a5e2193f54888751680c
dinThea <iagoelifa@gmail.com>
2019-01-04 03:16:29 -0200

    merge

--- auth/database/index.js
+++ auth/database/index.js
@@ -1,28 +1,56 @@
-'use strict'
+const es = require('elasticsearch')
+const { es_config } = require('../config')
+const db = new es.Client(es_config)
+const _ = require('lodash')
 
-const mongoose = require('mongoose')
-const config = require('../config')
-
-const PersonModel = require('./models/person')
-const PersonStorage = require('./storages/person')
-const PersonRepository = require('./repositories/person')
-
-const factory = () => {
-  const connection = mongoose.createConnection(config.MONGO_DB_URI)
-
-  const models = {
-    person: PersonModel.factory(connection)
-  }
-
-  const repositories = {
-    person: new PersonRepository(models.person)
-  }
-
-  const storages = {
-    person: new PersonStorage(models.person)
-  }
-
-  return { repositories, storages }
+var repositories = {
+	person: {
+		async findByEmail(email) {
+			try {
+				let result = await db.search({
+					index: 'person',
+					type: '_doc',
+					body: {
+						query: {
+							term: { email },
+						},
+					},
+				})
+				let found = _.take(result.hits.hits)[0] || null
+				if (found) found = found._source
+				return found || null
+			} catch (e) {
+				console.log('error find', e)
+				return e
+			}
+		},
+	},
 }
 
-module.exports = { factory }
+var storages = {
+	person: {
+		create(name, pass, email, college_id) {
+			try {
+				return db.index({
+					index: 'person',
+					type: '_doc',
+					body: {
+						name,
+						pass,
+						email,
+						college_id,
+					},
+				})
+			} catch (e) {
+				console.log('error create', e)
+				return e
+			}
+		},
+	},
+}
+
+module.exports = {
+	factory() {
+		return { repositories, storages }
+	},
+}
