0fdcd200ad0ca590041aa57242ba2fe70ba84cf6
henry <henriquecunhawd@gmail.com>
2019-01-06 18:20:35 -0200

    added log

--- middlewares/strategies/local.js
+++ middlewares/strategies/local.js
@@ -4,11 +4,12 @@ const { Strategy, ExtractJwt } = require('passport-jwt')
 const config = require('../../config')
 
 const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
-    secretOrKey: config.SECRET_JWT,
-    audience: 'client'
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: config.SECRET_JWT,
+	audience: 'client',
 }
 
 module.exports = new Strategy(opts, (jwt_payload, done) => {
-    return done(null, true)
+	console.log(jwt_payload)
+	return done(null, true)
 })
